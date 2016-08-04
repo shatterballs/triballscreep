27a60b424a9e0d43abc0d07195c6a945d6c8cc58
shatterballs <appleburps@gmail.com>
2016-08-04 13:35:11 +0800

    Update role.harvester.js

--- screeps.com/default/role.harvester.js
+++ screeps.com/default/role.harvester.js
@@ -6,7 +6,23 @@
  * var mod = require('role.harvester');
  * mod.thing == 'a thing'; // true
  */
-var harvester = 
+var roleHarvester = {
+   //this is a function within the rolHarvester object. Can be called by roleHarvester.run(creep)
+  run: function(creep){
+    //might have to use getKeys() in myFunction to see what's in the creep(?)
+    //1. go to energy source and extract if carrying<carryingcap
+    //2. go back to spawn/ other structures to dump energy
+    if(_.sum(creep.carry) < creep.carryCapacity){//check if maxed/not
+      //find nearest source and set to target
+      var sources = creep.room.find(FIND_SOURCES);
+      for(var closestSource in sources){
+        
+      }
+      //go to nearest source
+      
+    }
+  }
+}
 
 
 
