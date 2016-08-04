e9ea86610196ff17039885bc6eef0e67b8d048c1
shatterballs <appleburps@gmail.com>
2016-08-04 12:17:16 +0800

    Update main.js

--- screeps.com/default/main.js
+++ screeps.com/default/main.js
@@ -8,12 +8,23 @@ harvester from spawn
 var stage = "setup";
 if(stage=="setup")
 {
-    if(Game.spawns['Spawn1'].canCreateCreep([WORK, MOVE, CARRY], null, {role: 'harvester'}) == 0){ 
-        Game.spawns['Spawn1'].createCreep([WORK, MOVE, CARRY], null, {role: 'harvester'});
+    /*SPAWN CONTROL*/
+    if(_.filter(Game.creeps, ((creep)=>creep.Role.memory == 'Harvester')).length < 6){
+        if(Game.spawns['Spawn1'].canCreateCreep([WORK, MOVE, CARRY], null, {role: 'harvester'}) == 0){ 
+            Game.spawns['Spawn1'].createCreep([WORK, MOVE, CARRY], null, {role: 'harvester'});
+        }
+    }
+    else //more than 5 harvesters
+    {
         
     }
-    else
-    {
+    /*CREEP ACTION CONTROL*/
+    //loop through all the properties in an object, in this case, object is the Game.creeps, and it contains all the creep objects in game.
+    for(var NAME in Game.creeps){
+        if(Game.creeps[NAME].memory.Role=='Harvester'){
+            //run script here for creeps with harvester role
+        }
+    }
     }
 }
     
