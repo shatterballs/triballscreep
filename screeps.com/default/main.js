f09bc151035874d0f733c396b368bce849fd4ad0
shatterballs <appleburps@gmail.com>
2016-08-05 11:16:16 +0800

    Update main.js

--- screeps.com/default/main.js
+++ screeps.com/default/main.js
@@ -1,4 +1,3 @@
-module.exports.loop = function () {
 /*to do
 >>CURRENT GOALS: setup=>defend=>attack<< 
 Setup stage:
@@ -9,33 +8,34 @@ SPAWN   ->  harvesters   -   CHECK
                 (moveByPath for roads?)
         -> upgraders
 ROLE
-
 */
 
-var stage = "setup";
-if(stage=="setup")
-{
-    /*SPAWN CONTROL*/
-    if(_.filter(Game.creeps, ((creep)=>creep.Role.memory == 'Harvester')).length < 6){
-        if(Game.spawns['Spawn1'].canCreateCreep([WORK, MOVE, CARRY], null, {role: 'harvester'}) == 0){ 
-            Game.spawns['Spawn1'].createCreep([WORK, MOVE, CARRY], null, {role: 'harvester'});
-        }
-    }
-    else{ //more than 5 harvesters, build some builders, then some extenstions (to create creeps with more modules)
+var roleharvester = require("role.harvester");
 
-    }
-    /*CREEP ACTION CONTROL*/
-    //loop through all the properties in an object, in this case, object is the Game.creeps, and it contains all the creep objects in game.
-    for(var NAME in Game.creeps){
-        var creep = Game.creeps[NAME];
-        if(creep.memory.Role=='Harvester'){
-            //run script here for creeps with harvester role
-        }
-    }
-    }
-}
-    
-//harvester count
+module.exports.loop = function () {
 
-    
+        var stage = "setup";
+        if(stage=="setup")
+        {
+            /*SPAWN CONTROL*/
+            if(_.filter(Game.creeps, (creep) => creep.memory.role == 'harvester').length < 6){
+                if(Game.spawns['Spawn1'].canCreateCreep([WORK, MOVE, CARRY], null, {role: 'harvester'}) == 0){ 
+                    Game.spawns['Spawn1'].createCreep([WORK, MOVE, CARRY], null, {role: 'harvester'});
+                }
+            }
+            else{ //more than 5 harvesters, build some builders, then some extenstions (to create creeps with more modules)
+            }
+            /*CREEP ACTION CONTROL*/
+            //loop through all the properties in an object, in this case, object is the Game.creeps, and it contains all the creep objects in game.
+            for(var NAME in Game.creeps){
+                if(_.filter(Game.creeps, (creep) => creep.memory.role == 'harvester').length == 0){
+                    break;
+                }
+                var creep = Game.creeps[NAME];
+                if(creep.memory.role=='harvester'){
+                    //run script here for creeps with harvester role
+                    roleharvester.run(creep);
+                }
+            }
+        }
 }
