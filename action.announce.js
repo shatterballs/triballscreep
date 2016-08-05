d035a2606744d8ae764ae8d03362490210bbc880
shatterballs <appleburps@gmail.com>
2016-08-06 04:43:10 +0800

    Delete action.announce.js

--- action.announce.js
+++
@@ -1,8 +0,0 @@
-var actionAnnounce = {
-    run: function(creep){
-        var phrase = ["#CREEPS","LIVES","MATTER"]
-        creep.say(phrase[Game.time % phrase.length])
-    }
-}
-
-module.exports = actionAnnounce;
