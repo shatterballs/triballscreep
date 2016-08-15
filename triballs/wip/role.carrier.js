4f31d6be66b935d0168d88ddad5a0d681ad90d55
shatterballs <appleburps@gmail.com>
2016-08-15 09:42:05 +0800

    Create role.carrier.js

---
+++ triballs/wip/role.carrier.js
@@ -0,0 +1,35 @@
+/*
+ * Module code goes here. Use 'module.exports' to export things:
+ * module.exports.thing = 'a thing';
+ *
+ * You can import it from another modules like this:
+ * var mod = require('role.harvester');
+ * mod.thing == 'a thing'; // true
+ */
+var myFunctions = require('my.Functions');
+ 
+var rolecarrier = {
+   //this is a function within the rolHarvester object. Can be called by roleHarvester.run(creep)
+  run: function(creep){
+    //might have to use getKeys() in myFunction to see what's in the creep(?)
+    //1. go to energy source and extract if carrying<carryingcap
+    //2. go back to spawn/ other structures to dump energy
+        if(creep.carry.energy < creep.carryCapacity){//check if maxed/not
+            var source = myFunctions.findclosest(creep, FIND_SOURCES);  
+         //myFunctions.findclosest(creep, FIND_SOURCES) should return source object that is closest to the creep
+         //check my.Functions.js to debug, lol
+            if(creep.harvest(source) == ERR_NOT_IN_RANGE){
+                creep.moveTo(source);
+            }
+        }else{
+            var target = myFunctions.findclosest(creep, FIND_MY_SPAWNS);
+            if(creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
+                creep.moveTo(target);
+            }
+        }
+    }
+}
+
+module.exports = rolecarrier;
+
+//lol need to check for constants
